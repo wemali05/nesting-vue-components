1354638256132ee991687a556c8dde899b34b782
wemali05 <alfredwemali05@gmail.com>
2019-07-11 11:28:54 +0300

    added props in the Ninja component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
    <app-header></app-header>
-   <app-ninja></app-ninja>
+   <app-ninja v-bind:ninjas="ninjas"></app-ninja>
    <app-footer></app-footer>
   </div>
 </template>
@@ -19,7 +19,14 @@ export default {
   },
   data () {
     return {
-    
+        ninjas: [
+                {name: 'Ryu', speciality: 'Vue Components', show: false},
+                {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
+                {name: 'Hitoshi', speciality: 'Click Events', show: false},
+                {name: 'Tango', speciality: 'Conditionals', show: false},
+                {name: 'Kami', speciality: 'Webpack', show: false},
+                {name: 'Yoshi', speciality: 'Data Diggin', show: false}
+            ]
     }
   }
 }
--- src/components/Ninjas.vue
+++ src/components/Ninjas.vue
@@ -13,16 +13,14 @@
 
 
 export default {
+  props:{
+    ninjas:{
+      type: Array
+    }
+  },
   data () {
     return {
-         ninjas: [
-                {name: 'Ryu', speciality: 'Vue Components', show: false},
-                {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
-                {name: 'Hitoshi', speciality: 'Click Events', show: false},
-                {name: 'Tango', speciality: 'Conditionals', show: false},
-                {name: 'Kami', speciality: 'Webpack', show: false},
-                {name: 'Yoshi', speciality: 'Data Diggin', show: false}
-            ]
+       
     }
   }
 }
